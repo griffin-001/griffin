b415f14a25fcc8b474f8d56b425c7d40e60ced66
jamesvinnicombe <80378047+jvinn@users.noreply.github.com>
2022-04-28 16:20:07 +1000

    maven plugins can now be parsed into a hashmap

--- griffin-backend/ReadXML.java
+++ griffin-backend/ReadXML.java
@@ -7,36 +7,55 @@ import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.util.HashMap;
+import org.junit.Assert;
 
 public class ReadXML {
+    private static String projectName = "";
+    private static HashMap<String,String> plugins = new HashMap<>();
+    private static HashMap<String,String> dependencies = new HashMap<>();
+    private static HashMap<String,String> properties = new HashMap<>();
 
     public static void main(String[] argv) {
         try {
-            File fXmlFile = new File("griffin-backend/pom.xml");
+            File xmlFile = new File("griffin-backend/pom.xml");
             DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-            Document doc = dBuilder.parse(fXmlFile);
-            doc.getDocumentElement().normalize();
-            printDocument(doc, System.out);
+            Document doc = dBuilder.parse(xmlFile);
 
+            getPlugins(doc);
+
+
+
+            // Print plugins HashMap
+            plugins.forEach((key, value) -> System.out.println(key + " " + value));
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public static void printDocument(Document doc, OutputStream out) throws IOException, TransformerException {
-        TransformerFactory tf = TransformerFactory.newInstance();
-        Transformer transformer = tf.newTransformer();
-        transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "no");
-        transformer.setOutputProperty(OutputKeys.METHOD, "xml");
-        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-        transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
-        transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
-        transformer.transform(new DOMSource(doc),
-                new StreamResult(new OutputStreamWriter(out, "UTF-8")));
+    public static void getPlugins(Document doc) {
+
+        // Finds all <plugin> elements in the XML file
+        NodeList pluginList = doc.getElementsByTagName("plugin");
+
+        // Get the name and version of each plugin
+        for(int i = 0; i < pluginList.getLength(); i++) {
+            Element element = (Element) pluginList.item(i);
+
+            NodeList nameNode = element.getElementsByTagName("artifactId");
+            NodeList versionNode = element.getElementsByTagName("version");
+
+            Element nameElement = (Element) nameNode.item(0);
+            Element versionElement = (Element) versionNode.item(0);
+
+            plugins.put(nameElement.getTextContent(), versionElement.getTextContent());
+        }
     }
 }
