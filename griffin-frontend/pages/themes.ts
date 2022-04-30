5f56897eef331aed314145b759e94b3d456ae7f1
Chuyang Chen <40534565+saddboys@users.noreply.github.com>
2022-04-30 18:07:15 +1200

    setup themes colour

---
+++ griffin-frontend/pages/themes.ts
@@ -0,0 +1,16 @@
+import { ThemeOptions } from '@material-ui/core/styles/createTheme';
+
+export const themeOptions: ThemeOptions = {
+    palette: {
+        type: 'light',
+        primary: {
+            main: '#0058a9',
+            light: '#00baf2',
+            dark: '#3f51b5',
+        },
+        background: {
+            default: '#ffffff',
+            paper: '#f4f4f4',
+        },
+    },
+};
