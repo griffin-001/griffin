fb07f9ca2fc65e394c6658a2b90a702db62f63b1
Thomas Woodruff <twoodruff@student.unimelb.edu.au>
2022-05-02 16:50:01 +1000

    Refactor directory structure.

--- griffin-backend/src/main/java/com/griffin/griffinbackend/Application.java
+++ griffin-backend/src/main/java/com/griffin/griffinbackend/Application.java
@@ -1,13 +1,13 @@
-package com.example.griffinbackend;
+package com.griffin.griffinbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
--- griffin-backend/src/test/java/com/griffin/griffinbackend/ApplicationTests.java
+++ griffin-backend/src/test/java/com/griffin/griffinbackend/ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.griffinbackend;
+package com.griffin.griffinbackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
