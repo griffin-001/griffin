ba0778b19551b71687886eaf18d4130ce1754aab
Yu Xuan <yxxua1@student.unimelb.edu.au>
2022-05-07 09:32:44 +0800

    parse gradle update

--- temp-backend/userstory_parse/ReadGradle.java
+++ temp-backend/userstory_parse/ReadGradle.java
@@ -1,14 +1,11 @@
-package userstory_parse;
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class ReadGradle {
 
-    private static final List<String> dependency = new LinkedList<>();
+    private static String projectName = "";
+//    private static final List<String> dependency = new LinkedList<>();
+    private static final HashSet<String> dependencies = new HashSet<>();
 
     public static void main(String[] args) {
         //file package
@@ -17,17 +14,37 @@ public class ReadGradle {
         File[] files = fileList.listFiles();
         for (File file : files) {
             String filePath = file.toString();
+            File realFile = new File(filePath);
+            if(filePath.endsWith("settings.gradle")){
+                readProjectName(realFile);
+            }
             if(filePath.endsWith("gradle")){
-                File realFile = new File(filePath);
                 readGradle(realFile);
             }
         }
 
+        System.out.println("\nProjectName: ");
+        System.out.println(projectName);
         System.out.println("\nDependencies: ");
-        for (String s : dependency) {
+        for (String s : dependencies) {
             System.out.println(s);
         }
     }
+
+    private static void readProjectName(File file) {
+            try {
+                BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+                String line;
+                while ((line = br.readLine()) != null) {
+                    line=line.trim();
+                    if(line.contains("=")){
+                        projectName = line.split("=")[1].replace("'","").trim();
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
 
     private static void readGradle(File file) {
         try{
@@ -36,10 +53,32 @@ public class ReadGradle {
             while((line=br.readLine())!=null){
                 line = line.trim();
                 if(line.length() != 0){
-                    //Exclude lengths less than 5 { or }
-                    if(line.length()>5){
+                    //Exclude lengths less than 3 { or }
+                    if(line.length()>3){
                         //Excluding comments
                         if(line.charAt(0)!='/' && line.charAt(0)!='*'){
+                            //style 3
+                            if(line.startsWith("id")){
+                                if(line.contains("'")){
+                                    String[] split = line.split("'");
+                                    if(split.length>=3){
+//                                        dependency.add("NoGroup"+";"+split[1]+";"+split[3]);
+                                        dependencies.add("NoGroup"+";"+split[1]+";"+split[3]);
+                                    }else if(split.length<3){
+//                                        dependency.add("NoGroup"+";"+split[1]+";"+"latest");
+                                        dependencies.add("NoGroup"+";"+split[1]+";"+"latest");
+                                    }
+                                }else if(line.contains("'")){
+                                    String[] split = line.split("\"");
+                                    if(split.length>=3){
+//                                        dependency.add("NoGroup"+";"+split[1]+";"+split[3]);
+                                        dependencies.add("NoGroup"+";"+split[1]+";"+split[3]);
+                                    }else if(split.length<3){
+//                                        dependency.add("NoGroup"+";"+split[1]+";"+"latest");
+                                        dependencies.add("NoGroup"+";"+split[1]+";"+"latest");
+                                    }
+                                }
+                            }
                             //Excluding those ending in) or {
                             if((line.charAt(line.length()-1)!=')') && (line.charAt(line.length()-1)!='{')){
                                 //Exclude those that do not end with ' "
@@ -50,13 +89,18 @@ public class ReadGradle {
                                         if(line.contains(":")){
                                             String[] split = line.replace("'","").replace("\"","").split(":");
                                             if(split.length>2){
-                                                dependency.add(split[0]+";"+split[1]+";"+split[2]);
+//                                                dependency.add(split[0]+";"+split[1]+";"+split[2]);
+                                                dependencies.add(split[0]+";"+split[1]+";"+split[2]);
+                                            }else if(split.length==2){
+//                                                dependency.add(split[0]+";"+split[1]+";"+"latest");
+                                                dependencies.add(split[0]+";"+split[1]+";"+"latest");
                                             }
                                         }
                                         //style 2
                                     }else if(line.split(" ")[1].equals("group:")){
                                         String[] split = line.split("'");
-                                        dependency.add(split[1]+";"+split[3]+";"+split[5]);
+//                                        dependency.add(split[1]+";"+split[3]+";"+split[5]);
+                                        dependencies.add(split[1]+";"+split[3]+";"+split[5]);
                                     }
                                 }
                             }
