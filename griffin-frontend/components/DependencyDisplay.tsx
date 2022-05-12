340e9bff103542aa6878735faffcc002028d634d
Chuyang Chen <40534565+saddboys@users.noreply.github.com>
2022-05-12 12:08:46 +1200

    added card outline for dependency display

--- griffin-frontend/components/DependencyDisplay.tsx
+++ griffin-frontend/components/DependencyDisplay.tsx
@@ -5,7 +5,7 @@
 
 import React, { FunctionComponent } from 'react';
 import {Box} from "@mui/system";
-import {CardContent, Typography} from "@mui/material";
+import {Card, CardContent, Typography} from "@mui/material";
 
 interface OwnProps {
   item: {name: string, version: string, description: string}
@@ -17,17 +17,19 @@ const DependencyDisplay: FunctionComponent<Props> = (props) => {
 
   return (
       <Box>
-        <CardContent>
-          <Typography variant="h5" component="div">
-            {props.item.name}
-          </Typography>
-          <Typography sx={{ mb: 1.5 }}>
-            Version: {props.item.version}
-          </Typography>
-          <Typography variant="body2">
-            {props.item.description}
-          </Typography>
-        </CardContent>
+        <Card variant="outlined">
+          <CardContent>
+            <Typography variant="h5" component="div">
+              {props.item.name}
+            </Typography>
+            <Typography sx={{ mb: 1.5 }}>
+              Version: {props.item.version}
+            </Typography>
+            <Typography variant="body2">
+              {props.item.description}
+            </Typography>
+          </CardContent>
+        </Card>
       </Box>);
 };
 
