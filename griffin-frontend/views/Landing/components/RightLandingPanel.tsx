88bb8ab83ec49b4a3d58a6d64dc15ec6c234aa33
Glenn Phillips <glennph@yahoo.com.au>
2022-05-16 19:10:59 +1000

    landing page fix up and added link to dashboard

--- griffin-frontend/views/Landing/components/RightLandingPanel.tsx
+++ griffin-frontend/views/Landing/components/RightLandingPanel.tsx
@@ -33,7 +33,7 @@ const RightLandingPanel: FunctionComponent<Props> = (props) => {
             <Button
               variant="contained"
               onClick={() => {
-                router.push("/vulnerabilitiesReport")
+                router.push("/projectsDashboard")
                   .catch((e) => {
                     //error
 
