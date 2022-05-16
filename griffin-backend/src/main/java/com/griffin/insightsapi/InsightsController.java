1c3a1e3c185f274a01f5632678710b6846288324
Thomas Woodruff <twoodruff@student.unimelb.edu.au>
2022-05-16 18:40:54 +1000

    Remove unused code from last sprint and setup insightsapi package.

---
+++ griffin-backend/src/main/java/com/griffin/insightsapi/InsightsController.java
@@ -0,0 +1,12 @@
+package com.griffin.insightsapi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class InsightsController {
+
+    @GetMapping("/basicinfo")
+    public String get
+    
+}
