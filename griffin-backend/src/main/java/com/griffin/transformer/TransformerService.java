aced961d9f6431bc225871d6c7ac881192bbc39b
Thomas Woodruff <twoodruff@student.unimelb.edu.au>
2022-05-17 14:02:30 +1000

    Remove imports.

--- griffin-backend/src/main/java/com/griffin/transformer/TransformerService.java
+++ griffin-backend/src/main/java/com/griffin/transformer/TransformerService.java
@@ -7,15 +7,11 @@ import com.griffin.insightsdb.model.Repository;
 import com.griffin.insightsdb.service.InsightDBService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
