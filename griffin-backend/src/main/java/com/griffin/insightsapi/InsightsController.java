1a514a5388ce8b139d06a0d8321676a7d467d9e0
jamesvinnicombe <80378047+jvinn@users.noreply.github.com>
2022-05-22 17:45:30 +1000

    log4j endpoints now stored in arrays

--- griffin-backend/src/main/java/com/griffin/insightsapi/InsightsController.java
+++ griffin-backend/src/main/java/com/griffin/insightsapi/InsightsController.java
@@ -17,9 +17,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
 import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
@@ -43,13 +40,14 @@ public class InsightsController {
     }
 
     @GetMapping("/before")
-    public ResponseEntity<JSONObject> before() {
+    public ResponseEntity<JSONArray> before() {
         JSONObject summary = new JSONObject();
-        summary.put("unresolvedExistingVulnerabilities", 0);
-        summary.put("resolvedExistingVulnerabilities", 3);
-        summary.put("projectsAffected", 0);
+        summary.put("newVulnerabilities", 0);
+        summary.put("unresolvedExistingVulnerabilities", 3);
+        summary.put("resolvedExistingVulnerabilities", 0);
+        summary.put("projectsAffected", 1);
         summary.put("totalProjects", 1);
-        summary.put("repositoriesAffected", 0);
+        summary.put("repositoriesAffected", 1);
         summary.put("totalRepositories", 1);
 
         JSONArray data = new JSONArray();
@@ -59,7 +57,7 @@ public class InsightsController {
         p1.put("repoName", "assertj-core");
         p1.put("dependencyName", "org.eclipse.jgit");
         p1.put("dependencyVersion", "6.1.0.202203080745-r");
-        p1.put("vulnerabilityStatus", "resolved");
+        p1.put("vulnerabilityStatus", "unresolved");
         data.add(p1);
 
         JSONObject p2 = new JSONObject();
@@ -67,7 +65,7 @@ public class InsightsController {
         p2.put("repoName", "assertj-core");
         p2.put("dependencyName", "org.springframework.boot:spring-boot-starter");
         p2.put("dependencyVersion", "2.7.0");
-        p2.put("vulnerabilityStatus", "resolved");
+        p2.put("vulnerabilityStatus", "unresolved");
         data.add(p2);
 
         JSONObject p3 = new JSONObject();
@@ -75,25 +73,27 @@ public class InsightsController {
         p3.put("repoName", "assertj-core");
         p3.put("dependencyName", "org.springframework.boot:spring-boot-starter-web");
         p3.put("dependencyVersion", "2.7.0");
-        p3.put("vulnerabilityStatus", "resolved");
+        p3.put("vulnerabilityStatus", "unresolved");
         data.add(p3);
 
-        OffsetDateTime now = OffsetDateTime.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ISO_DATE_TIME;
-        String date = formatter.format(now);
+        String date = "2022-05-22T06:49:04.971978458Z";
 
         JSONObject response = new JSONObject();
         response.put("date", date);
         response.put("summary", summary);
         response.put("data", data);
 
-        return new ResponseEntity<>(response, HttpStatus.OK);
+        JSONArray responseArray = new JSONArray();
+        responseArray.add(response);
+
+        return new ResponseEntity<>(responseArray, HttpStatus.OK);
     }
 
     @GetMapping("/after")
-    public ResponseEntity<JSONObject> after() {
+    public ResponseEntity<JSONArray> after() {
         JSONObject summary = new JSONObject();
-        summary.put("unresolvedExistingVulnerabilities", 1);
+        summary.put("newVulnerabilities", 1);
+        summary.put("unresolvedExistingVulnerabilities", 0);
         summary.put("resolvedExistingVulnerabilities", 3);
         summary.put("projectsAffected", 1);
         summary.put("totalProjects", 1);
@@ -143,7 +143,55 @@ public class InsightsController {
         response.put("summary", summary);
         response.put("data", data);
 
-        return new ResponseEntity<>(response, HttpStatus.OK);
+        // -----------------------------
+
+        JSONObject summary2 = new JSONObject();
+        summary2.put("newVulnerabilities", 0);
+        summary2.put("unresolvedExistingVulnerabilities", 3);
+        summary2.put("resolvedExistingVulnerabilities", 0);
+        summary2.put("projectsAffected", 1);
+        summary2.put("totalProjects", 1);
+        summary2.put("repositoriesAffected", 1);
+        summary2.put("totalRepositories", 1);
+
+        JSONArray data2 = new JSONArray();
+
+        JSONObject p12 = new JSONObject();
+        p12.put("projectName", "assertj");
+        p12.put("repoName", "assertj-core");
+        p12.put("dependencyName", "org.eclipse.jgit");
+        p12.put("dependencyVersion", "6.1.0.202203080745-r");
+        p12.put("vulnerabilityStatus", "unresolved");
+        data2.add(p12);
+
+        JSONObject p22 = new JSONObject();
+        p22.put("projectName", "assertj");
+        p22.put("repoName", "assertj-core");
+        p22.put("dependencyName", "org.springframework.boot:spring-boot-starter");
+        p22.put("dependencyVersion", "2.7.0");
+        p22.put("vulnerabilityStatus", "unresolved");
+        data2.add(p22);
+
+        JSONObject p32 = new JSONObject();
+        p32.put("projectName", "assertj");
+        p32.put("repoName", "assertj-core");
+        p32.put("dependencyName", "org.springframework.boot:spring-boot-starter-web");
+        p32.put("dependencyVersion", "2.7.0");
+        p32.put("vulnerabilityStatus", "unresolved");
+        data2.add(p32);
+
+        String date2 = "2022-05-22T06:49:04.971978458Z";
+
+        JSONObject response2 = new JSONObject();
+        response2.put("date", date2);
+        response2.put("summary", summary2);
+        response2.put("data", data2);
+
+        JSONArray responseArray = new JSONArray();
+        responseArray.add(response);
+        responseArray.add(response2);
+
+        return new ResponseEntity<>(responseArray, HttpStatus.OK);
     }
 
 
