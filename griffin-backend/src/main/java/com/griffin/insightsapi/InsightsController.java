34bdf2aa3c9eea4ff5917e5c6ee74ec2edc48fc3
jamesvinnicombe <80378047+jvinn@users.noreply.github.com>
2022-05-22 12:28:17 +1000

    added endpoint which returns scan with log4j

--- griffin-backend/src/main/java/com/griffin/insightsapi/InsightsController.java
+++ griffin-backend/src/main/java/com/griffin/insightsapi/InsightsController.java
@@ -1,12 +1,24 @@
 package com.griffin.insightsapi;
 
+import com.griffin.cve.Vulnerability;
+import com.griffin.cve.response.ScanResponse;
 import com.griffin.insightsdb.model.Dependency;
+import com.griffin.insightsdb.model.RepositorySnapShot;
+import com.griffin.insightsdb.model.TimeStamp;
+import com.griffin.insightsdb.repository.TimeStampRepository;
 import com.griffin.insightsdb.service.InsightDBService;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @RestController
@@ -19,12 +31,6 @@ public class InsightsController {
         this.insightDBService = insightDBService;
     }
 
-
-    @GetMapping("/basicinfo")
-    public String getSomething() {
-        return "Something";
-    }
-
     @GetMapping("/allDeps")
     public String allDeps() {
         logger.info("Getting all deps...");
@@ -33,4 +39,30 @@ public class InsightsController {
         return dependensies.toString();
     }
 
+    @GetMapping("/hardcodedScan")
+    public ResponseEntity<JSONObject> hardcodedScan() {
+        JSONObject summary = new JSONObject();
+        summary.put("unresolvedExistingVulnerabilities", 0);
+        summary.put("resolvedExistingVulnerabilities", 0);
+        summary.put("projectsAffected", 1);
+        summary.put("totalProjects", 1);
+        summary.put("repositoriesAffected", 1);
+        summary.put("totalRepositories", 1);
+
+        JSONArray data = new JSONArray();
+        JSONObject project = new JSONObject();
+        project.put("projectName", "assertj");
+        project.put("repoName", "assertj-core");
+        project.put("dependencyName", "org.apache.logging.log4j");
+        project.put("dependencyVersion", "2.13.0");
+        project.put("vulnerabilityStatus", "unresolved");
+        data.add(project);
+
+        JSONObject response = new JSONObject();
+        response.put("date", "2022-05-22 11:30:21.155");
+        response.put("summary", summary);
+        response.put("data", data);
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
 }
